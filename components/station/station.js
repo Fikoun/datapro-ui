90d89a5bbd0f7b5df8b6e7215191d7ce07c38903
Fikoun <fikoun@icloud.com>
2021-05-28 15:46:29 +0200

    debug option

--- components/station/station.js
+++ components/station/station.js
@@ -11,10 +11,15 @@ export default function Station({ station, clickHandle}) {
     // context -> stanice (data, handles)
     const {name, state} = station;
 
+    const click = () => {
+        console.log("Switchin to", station);
+        clickHandle(station)
+    }
+
     return (
         <div
             className="rounded-2xl px-6 py-4 m-4 bg-white shadow hover:shadow-md hover:-translate-y-2"
-            onClick={()=> clickHandle(station)}
+            onClick={click}
         >
             <h2>{name}</h2>
             
